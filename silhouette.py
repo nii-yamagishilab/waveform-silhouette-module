89dd154bc15c12ccefed6e00b38a95ddff217d2b
Luong Hieu Thi <luonghieuthi@gmail.com>
2022-05-09 11:43:38 +0900

    WaveformSilhouette and LinearEncoding modules

---
+++ silhouette.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn.functional as F
+
+
+class WaveformSilhouette(torch.nn.Module):
+  def __init__(
+    self,
+    win_length,
+    hop_length
+  ):
+    super(WaveformSilhouette, self).__init__()
+    self.maxpool = torch.nn.MaxPool1d(win_length, stride=hop_length, padding=(win_length - hop_length)//2)
+
+  def forward(self, waveform):
+    featmax = self.maxpool(waveform)
+    featmin = -self.maxpool(-waveform)
+    feat = torch.cat((featmax,featmin), axis=-2)
+    return feat
+
+
+class LinearEncoding(torch.nn.Module):
+  def __init__(
+    self,
+    quantization_channels=256
+  ):
+    super(LinearEncoding, self).__init__()
+    self.quantization_channels = quantization_channels
+
+  def forward(self, waveform):
+    safe_waveform = torch.minimum(torch.abs(waveform), waveform*0.0 + 1.0)
+    signal = (torch.sign(waveform)*safe_waveform+1) / 2
+    signal = signal * (self.quantization_channels-1) + 0.5
+    return signal.long()
